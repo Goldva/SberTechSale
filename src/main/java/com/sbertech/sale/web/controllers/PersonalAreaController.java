8ce0bbd516c9c7e63a687ad256d77f6a47947918
Goldva <zovode@gmail.com>
2017-02-06 19:18:27 +0400

    Commit

---
+++ src/main/java/com/sbertech/sale/web/controllers/PersonalAreaController.java
@@ -0,0 +1,27 @@
+package com.sbertech.sale.web.controllers;
+
+import com.sbertech.sale.data.User;
+import com.sbertech.sale.service.BidService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes("loginUser")
+@RequestMapping(value = "/personalArea")
+public class PersonalAreaController {
+    @Autowired
+    private BidService bidService;
+
+    @RequestMapping()
+    public ModelAndView personalArea(@ModelAttribute("loginUser") User user){
+        ModelAndView model = new ModelAndView();
+        model.addObject("bids", bidService.getAllBidsForUser(user));
+        model.setViewName("personalArea");
+        return model;
+
+    }
+}
