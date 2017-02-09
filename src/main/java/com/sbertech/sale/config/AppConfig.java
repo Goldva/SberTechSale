ce08f64044715eee128c5d48b3f9a4e254d006da
Goldva <zovode@gmail.com>
2017-02-09 15:48:58 +0400

    End of work

--- src/main/java/com/sbertech/sale/config/AppConfig.java
+++ src/main/java/com/sbertech/sale/config/AppConfig.java
@@ -41,7 +41,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public BasicDataSource dataSource() {
         BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName("org.hsqldb.jdbcDriver");
-        ds.setUrl("jdbc:hsqldb:D:\\Java\\SberTechSale\\MyDb\\mydb.db");
+        ds.setUrl("jdbc:hsqldb:MyDb\\mydb.db");
         ds.setUsername("sa");
         ds.setPassword("");
         return ds;
